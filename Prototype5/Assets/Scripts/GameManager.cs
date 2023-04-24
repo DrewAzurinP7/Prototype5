0835363bf8331e4382e9c6e4f0d24b69a3806c70
DrewAzurinP7 <740723@student.dusd.net>
2023-04-24 15:27:17 -0700

    Completed Unit 5 & started Bonus Challenges
    Completed Unit 5 & started Bonus Challenges

--- Prototype5/Assets/Scripts/GameManager.cs
+++ Prototype5/Assets/Scripts/GameManager.cs
@@ -8,24 +8,48 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     public TextMeshProUGUI scoreText;
-    public List<GameObject> targets;
+    public TextMeshProUGUI livesText;
     public TextMeshProUGUI gameOverText;
-    public bool isGameActive;
+    public List<GameObject> targets;
+    public GameObject titleScreen;
+    public GameObject pauseScreen;
     public Button restartButton;
+    public bool isGameActive;
+
     private float spawnRate = 1.0f;
     private int score;
+    private int lives;
+    private bool paused;
 
     // Start is called before the first frame update
-    void Start()
+    public void StartGame(int difficulty)
     {
         isGameActive = true;
+        score = 0;
+        spawnRate = spawnRate / difficulty;
 
         StartCoroutine(SpawnTarget());
-        score = 0;
         UpdateScore(0);
+        UpdateLives(3);
 
+        titleScreen.gameObject.SetActive(false);
     }
 
+    void ChangePaused()
+    {
+        if (!paused)
+        {
+            paused = true;
+            pauseScreen.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else
+        {
+            paused = false;
+            pauseScreen.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
     public void GameOver()
     {
         gameOverText.gameObject.SetActive(true);
@@ -48,8 +72,27 @@ public class GameManager : MonoBehaviour
         scoreText.text = "Score: " + score;
     }
 
+    public void UpdateLives(int livesToChange)
+    {
+        lives += livesToChange;
+        livesText.text = "Lives: " + lives;
+        if(lives <= 0)
+        {
+            GameOver();
+        }
+    }
+
     public void RestartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    void Update()
+    {
+        //Check if the user has press the P key
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            ChangePaused();
+        }
     }
 }
